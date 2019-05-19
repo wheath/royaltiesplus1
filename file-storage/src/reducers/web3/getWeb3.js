b5f6ec93283c9d2126c057695872541aeb9f13fb
Tim Heath <theath@terrafugia.com>
2019-05-18 23:05:44 -0400

    initial static version

---
+++ file-storage/src/reducers/web3/getWeb3.js
@@ -0,0 +1,26 @@
+import store from '../../store'
+import Web3 from 'web3'
+
+import FilestorageClient from '@skalenetwork/filestorage.js';
+
+export const WEB3_INITIALIZED = 'WEB3_INITIALIZED'
+function web3Initialized(results) {
+  return {
+    type: WEB3_INITIALIZED,
+    payload: results
+  }
+}
+
+export let getWeb3 = new Promise(function(resolve, reject) {
+  let web3 = new Web3(process.env.SKALE_CHAIN);
+  let web3files = window.web3;	 
+
+  //let filestorage = new FilestorageClient(process.env.SKALE_CHAIN, true);
+  let filestorage = new FilestorageClient(web3files, true);
+    
+  resolve(store.dispatch(web3Initialized({
+    web3: web3, 
+    account: process.env.ACCOUNT, 
+    filestorage: filestorage
+  })));
+})
